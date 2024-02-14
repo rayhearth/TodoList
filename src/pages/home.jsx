aeb0bb06e901c566cec98a18e91a51023684685f
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-02-14 18:39:46 +0100

    add img to homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Images from '../assets/img/Images';
+import img_accueil from '../assets/img/character_acc.png'
 
 
 
 const Home = () => {
   return (
     <div>
-      <img src={Images.image1} alt="" />
+      <img src={img_accueil} className="logo react" alt="React logo" />
       <h1>To-Do List</h1>
       <p>Boostez votre productivité avec notre application de gestion de tâches dynamique ! Que vous soyez un professionnel multitâche ou un étudiant organisé, simplifiez votre quotidien en planifiant, éditant et accomplissant vos tâches en un clin d'œil. Restez agile, restez organisé, et atteignez vos objectifs avec facilité !</p>
     </div>
@@ -14,3 +14,6 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
