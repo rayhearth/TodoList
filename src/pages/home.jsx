65f34ab0dacd54dd94a8670e7fd5e892b8a0a690
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-02-15 18:40:25 +0100

    adjust section home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import img_accueil from '../assets/img/character_acc.png'
-import start from '../assets/img/start.svg'
-
-
+import React from "react";
+import img_accueil from "../assets/img/character_acc.png";
+import start from "../assets/img/start.svg";
 
 const Home = () => {
-  return (
-    <div>
-      <img src={img_accueil} className="logo react" alt="React logo" />
-      <h1>To-Do List</h1>
-      <p>Boostez votre productivité avec notre application de gestion de tâches dynamique ! Que vous soyez un professionnel multitâche ou un étudiant organisé, simplifiez votre quotidien en planifiant, éditant et accomplissant vos tâches en un clin d'œil. Restez agile, restez organisé, et atteignez vos objectifs avec facilité !</p>
-      <button>Let's start<img src={start} alt="" /></button>
-    </div>
-  );
+	return (
+		<section className="home">
+			<img src={img_accueil} className="logo react" alt="React logo" />
+			<h1>To-Do List</h1>
+			<p>
+				Boostez votre productivité avec notre application de gestion de tâches
+				dynamique ! Que vous soyez un professionnel multitâche ou un étudiant
+				organisé, simplifiez votre quotidien en planifiant, éditant et
+				accomplissant vos tâches en un clin d'œil. Restez agile, restez
+				organisé, et atteignez vos objectifs avec facilité !
+			</p>
+			<button className="start-button">Let's start </button>
+		</section>
+	);
 };
 
 export default Home;
-
-
-
