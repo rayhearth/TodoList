bf621696c31757d7eecdf71fdba37565efa49166
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-02-15 18:03:59 +0100

    create sass for homepage and button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import img_accueil from '../assets/img/character_acc.png'
+import start from '../assets/img/start.svg'
 
 
 
@@ -9,6 +10,7 @@ const Home = () => {
       <img src={img_accueil} className="logo react" alt="React logo" />
       <h1>To-Do List</h1>
       <p>Boostez votre productivité avec notre application de gestion de tâches dynamique ! Que vous soyez un professionnel multitâche ou un étudiant organisé, simplifiez votre quotidien en planifiant, éditant et accomplissant vos tâches en un clin d'œil. Restez agile, restez organisé, et atteignez vos objectifs avec facilité !</p>
+      <button>Let's start<img src={start} alt="" /></button>
     </div>
   );
 };
