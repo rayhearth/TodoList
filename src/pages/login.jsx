109f445a279ac39776e1258679473ee2e0746497
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-02-16 18:10:48 +0100

    create form connect for login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>    login </h1>
-      
-    </div>
-  );
+	return (
+		<section className="login">
+			<div className="sign-in">
+				<h2> Connexion </h2>
+				<form action="">
+					<input type="text" id="mail-connect" />
+					<label htmlFor="mail-connect">Email address</label>
+					<input type="text" id="password-connect" />
+					<label htmlFor="password-connect">password</label>
+				</form>
+			</div>
+			<div className="sign-up">
+				<h2> Créer mon compte </h2>
+				<form action="">
+					<input type="text" id="mail-register" />
+					<label htmlFor="mail-register">Email address</label>
+					<input type="text" id="password-register" />
+					<label htmlFor="password-register">password</label>
+				</form>
+			</div>
+		</section>
+	);
 };
 
 export default Login;
