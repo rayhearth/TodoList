575d311b60a70383e29c0ff7b56846dc8edbdf39
User <emmanuellejupon@gmail.com>
2024-06-07 18:41:09 +0200

    add edit comportment in the todo

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,38 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleTodo, deleteTodo } from "../feature/todo.slice";
 
 
-const Todo = ({name, completed, id}) => {
+const Todo = ({ name, completed, id }) => {
+  const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState(name);
 
-	const dispatch = useDispatch();
+  const handleStatusChange = () => {
+    dispatch(toggleTodo(id));
+  };
 
-    const handleStatus = () => {
-        dispatch(toggleTodo(id)); // Utilisez l'id correct
-    };
+  const handleDelete = () => {
+    dispatch(deleteTodo(id));
+  };
 
-    const handleDelete = () => {
-        dispatch(deleteTodo(id)); // Utilisez l'id correct
-    };
-	
-	return (
-		<li className="todo stack-small">
-					<div className="c-cb">
-						<input id={id} type="checkbox" name={id} defaultChecked={completed} />
-						<label className="todo-label" htmlFor={id}>
-							{name}
-						</label>
-					</div>
-					<div className="btn-group">
-						<button type="button" className="btn">
-							Éditer <span className="visually-hidden">Manger</span>
-						</button>
-						<button type="button" className="btn btn__danger">
-							Supprimer <span className="visually-hidden">Manger</span>
-						</button>
-					</div>
-				</li>
-	);
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    dispatch(editTodo({ id, name: newName }));
+    setIsEditing(false);
+  };
+
+  return (
+    <li className="todo stack-small">
+      {isEditing ? (
+        <form onSubmit={handleEditSubmit}>
+          <div className="c-cb">
+            <input
+              id={id}
+              type="text"
+              value={newName}
+              onChange={handleNameChange}
+              className="input input__lg"
+            />
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn" onClick={() => setIsEditing(false)}>
+              Annuler
+            </button>
+            <button type="submit" className="btn btn__primary">
+              Sauvegarder
+            </button>
+          </div>
+        </form>
+      ) : (
+        <>
+          <div className="c-cb">
+            <input id={id} type="checkbox" checked={!!completed} onChange={handleStatusChange} />
+            <label className="todo-label" htmlFor={id}>
+              {name}
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn" onClick={() => setIsEditing(true)}>
+              Éditer <span className="visually-hidden">{name}</span>
+            </button>
+            <button type="button" className="btn btn__danger" onClick={handleDelete}>
+              Supprimer <span className="visually-hidden">{name}</span>
+            </button>
+          </div>
+        </>
+      )}
+    </li>
+  );
 };
-
 export default Todo;
--- src/feature/todo.slice.jsx
+++ src/feature/todo.slice.jsx
@@ -1,35 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	todos:[
-	{ id: 1, name: "Create a todolist", completed: "false" },
-	{ id: 2, name: "update a todolist", completed: "true" },
-	{ id: 3, name: "delete todos", completed: "true" },
-],
-inputValue:""
+	todos: [
+		{ id: 1, name: "Create a todolist", completed: "false" },
+		{ id: 2, name: "update a todolist", completed: "true" },
+		{ id: 3, name: "delete todos", completed: "true" },
+	],
+	inputValue: "",
 };
 
 export const todoSlice = createSlice({
-	name: "todos",
-	initialState,
-	reducers: {
-		addTodo: (state, action) => {
-			state.push(action.payload);
-		},
-		toggleTodo: (state, action) => {
-			const todo = state.find((todo) => todo.id === action.payload);
-			if (todo) {
-				todo.completed = !todo.completed;
-			}
-		},
-		deleteTodo: (state, action) => {
-			return state.filter((todo) => todo.id !== action.payload);
-		},
-		setInputValue:(state, action) => {
-			state.inputValue = action.payload;
-		}
-	},
+  name: "todos",
+  initialState,
+  reducers: {
+    setInputValue: (state, action) => {
+      state.inputValue = action.payload;
+    },
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const { id, name } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.name = name;
+      }
+    },
+  },
 });
 
-export const { addTodo, toggleTodo, deleteTodo, setInputValue } = todoSlice.actions;
+export const { setInputValue, addTodo, toggleTodo, deleteTodo, editTodo } = todoSlice.actions;
 export default todoSlice.reducer;
